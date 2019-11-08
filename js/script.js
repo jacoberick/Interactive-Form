9e253f7247d4cfda266c465c8d21b79ecf1f4433
Jacob White <jacobewhitedev@gmail.com>
2019-11-07 20:13:19 -0600

    finalization of field validations

--- js/script.js
+++ js/script.js
@@ -35,6 +35,7 @@ $("#title")
   })
   .change();
 
+//T-SHIRT INFO
 // function `showShirtSelection` takes the selected design and only shows shirts related to that design
 let showShirtSelection = shirtDesign => {
   colorDropDown.removeAttr("disabled");
@@ -61,12 +62,14 @@ $("#design")
   })
   .change();
 
-//checkbox strikethrough and disable
+//ACTIVITY SECTION
+//`disableCheckbox` adds disabled attribute and strickthrough class
 let disableCheckbox = activity => {
   activity.attr({ disabled: true });
   activity.parent().addClass("strikethrough");
 };
 
+//`resetCheckbox` removes disabled attribute and strikethrough class
 let resetCheckbox = date => {
   date
     .removeAttr("disabled")
@@ -74,9 +77,9 @@ let resetCheckbox = date => {
     .removeClass("strikethrough");
 };
 
+//disables/resets conflicting activies for Tuesday 9-12 if selected
 tues9to12
   .change(function() {
-    tues9to12;
     resetCheckbox(tues9to12);
     if (jsFrameworks.prop("checked")) {
       disableCheckbox(express);
@@ -86,9 +89,9 @@ tues9to12
   })
   .change();
 
+//disables/resets conflicting activies for Tuesday 1-4 if selected
 tues1to4
   .change(function() {
-    tues1to4;
     resetCheckbox(tues1to4);
     if (node.prop("checked")) {
       disableCheckbox(jsLibraries);
@@ -113,25 +116,34 @@ $(allActivities).each(function() {
 //bill total appendage
 $(".activities").append(`<div>Total: <span id="total">$0</span></div>`);
 
-//only shows credit card inputs if credit card method is chosen
+//PAYMENT
+//payment info defaults to Credit Card
+//payment options show/hide based on payment selection
 selectPaymentMet.attr({ disabled: true });
-$("#payment").change(function() {
-  this.value == "Credit Card"
-    ? (credCardFields.show(), bitcoin.hide(), paypal.hide())
-    : credCardFields.hide();
-  this.value == "PayPal"
-    ? (paypal.show(), bitcoin.hide(), credCardFields.hide())
-    : paypal.hide();
-  this.value == "Bitcoin"
-    ? (bitcoin.show(), paypal.hide(), credCardFields.hide())
-    : bitcoin.hide();
-});
+$("#payment")
+  .change(function() {
+    this.value == "Credit Card"
+      ? (credCardFields.show(), bitcoin.hide(), paypal.hide())
+      : credCardFields.hide();
+    this.value == "PayPal"
+      ? (paypal.show(), bitcoin.hide(), credCardFields.hide())
+      : paypal.hide();
+    this.value == "Bitcoin"
+      ? (bitcoin.show(), paypal.hide(), credCardFields.hide())
+      : bitcoin.hide();
+  })
+  .change();
 
+//VALIDATIONS
 //flash error message function
 let flashErrorMessage = (form, message) => {
   $(".reqForm").remove();
   form.after(message);
-  $(".reqForm").fadeOut(6000);
+};
+
+//remove error message function
+let removeErrorMessage = form => {
+  $(".reqForm").remove();
 };
 
 //REGEX FUNCTIONS
@@ -162,85 +174,120 @@ let cvvValidation = cvv => {
 };
 
 //FEILD VALIDATIONS
-// name validation
-$("#name").blur(function() {
-  let name = $("#name").val();
-  const errorMessage = "<span class='reqForm'>This form is required</span>";
-  name.length > 0
-    ? $(this)
-        .removeClass("invalid")
-        .addClass("valid")
-    : ($(this)
-        .removeClass("valid")
-        .addClass("invalid"),
-      flashErrorMessage($(this), errorMessage));
-});
+let fieldValidate = () => {
+  $("form")
+    .find("input[type=text], input[type=email]")
+    .each((i, v) => {
+      let that = $(v);
+      let id = that.attr("id");
 
-// email validation
-$("#mail").blur(function() {
-  let email = $("#mail").val();
-  const errorMessage = "<span class='reqForm'>Email is not valid</span>";
-  emailValidation(email)
-    ? $(this)
-        .removeClass("invalid")
-        .addClass("valid")
-    : ($(this)
-        .removeClass("valid")
-        .addClass("invalid"),
-      flashErrorMessage($(this), errorMessage));
-});
+      //using switch statements to check field validity and provide error messages on click out of input field.
+      that.blur(() => {
+        let val = that.val();
+        let error, valid;
+        switch (id) {
+          case "name":
+            error = "This form field is required.";
+            valid = val.length > 0 ? true : false;
+            break;
+          case "mail":
+            error = "Email address is not valid.";
+            valid = emailValidation(val);
+            break;
+          case "other-title":
+            error = "This form field is required.";
+            valid = val.length > 0 ? true : false;
+            break;
+          case "cc-num":
+            error = "Credit card number is not valid.";
+            valid = ccNumValidation(val);
+            break;
+          case "zip":
+            error = "Zip code is not valid.";
+            valid = zipValidation(val);
+            break;
+          case "cvv":
+            error = "CVV is not valid.";
+            valid = cvvValidation(val);
+            break;
+          default:
+            error = "This form field is required.";
+        }
 
-//credit card validation
-$("#cc-num").blur(function() {
-  let ccNum = $("#cc-num").val();
-  const errorMessage =
-    "<span class='reqForm'>Credit Card number is not valid</span>";
-  ccNumValidation(ccNum)
-    ? $(this)
-        .removeClass("invalid")
-        .addClass("valid")
-    : ($(this)
-        .removeClass("valid")
-        .addClass("invalid"),
-      flashErrorMessage($(this), errorMessage));
-});
-
-//zip code validation
-$("#zip").blur(function() {
-  let zipVal = $("#zip").val();
-  const errorMessage = "<span class='reqForm'>Zip Code is not valid</span>";
-  zipValidation(zipVal)
-    ? $(this)
-        .removeClass("invalid")
-        .addClass("valid")
-    : ($(this)
-        .removeClass("valid")
-        .addClass("invalid"),
-      flashErrorMessage($(this), errorMessage));
-});
-
-//cvv code validation
-$("#cvv").blur(function() {
-  let cvvVal = $("#cvv").val();
-  const errorMessage = "<span class='reqForm'>CVV is not valid</span>";
-  cvvValidation(cvvVal)
-    ? $(this)
-        .removeClass("invalid")
-        .addClass("valid")
-    : ($(this)
-        .removeClass("valid")
-        .addClass("invalid"),
-      flashErrorMessage($(this), errorMessage));
-});
-
-//VALIDATION CONDITIONAL TEST
-let validationConditionalTEST = x => {
-  x
-    ? $(this)
-        .removeClass("invalid")
-        .addClass("valid")
-    : ($(this)
-        .removeClass("valid")
-        .addClass("invalid"),
-      flashErrorMessage($(this), errorMessage));
+        // dynamically show and hide error messages based on added/removed class
+        const message = `<span class='reqForm'>${error}</span>`;
+        valid
+          ? (that.removeClass("invalid").addClass("valid"),
+            removeErrorMessage(that))
+          : (that.removeClass("valid").addClass("invalid"),
+            flashErrorMessage(that, message));
+      });
+    });
 };
+fieldValidate();
+
+//adds class of invalid to offending input forms
+let invalidateOnSubmit = form => {
+  form.addClass("invalid");
+};
+
+// form submission
+$("form").submit(e => {
+  e.preventDefault();
+  let valid = true;
+
+  // check name not empty
+  $("#name").val().length
+    ? true
+    : ((valid = false), invalidateOnSubmit($("#name")));
+
+  // check valid email
+  emailValidation($("#mail").val())
+    ? console.log("mail pass")
+    : ((valid = false), invalidateOnSubmit($("#mail")));
+
+  //checks if 'other' job role is selected and has a value
+  if (
+    $("#title")
+      .val()
+      .toLowerCase() === "other"
+  ) {
+    otherTxtInput.val()
+      ? true
+      : ((valid = false), invalidateOnSubmit($("#other-title")));
+  }
+  // check that at least one activity is checked
+  $(".activities").find("input:checked").length
+    ? true
+    : ((valid = false),
+      $("#reviewFormActivity").remove(),
+      $("#activityLegend").after(
+        `<span id='reviewFormActivity'>Please select at least one activity</span>`
+      ));
+
+  // if credit card option selected, user enters cc num, zip, and cvv
+  if (
+    $("#payment")
+      .val()
+      .toLowerCase() === "credit card"
+  ) {
+    ccNumValidation($("#cc-num").val())
+      ? true
+      : ((valid = false), invalidateOnSubmit($("#cc-num")));
+    zipValidation($("#zip").val())
+      ? true
+      : ((valid = false), invalidateOnSubmit($("#zip")));
+    cvvValidation($("#cvv").val())
+      ? true
+      : ((valid = false), invalidateOnSubmit($("#cvv")));
+  }
+
+  // a valid submission triggers an alert notification and refreshes the page, while a failed submit highlights required fields in red and jumps to top of page.
+  valid
+    ? (alert("Successful Submit!"), location.reload())
+    : ($(window).scrollTop(0, 0),
+      $("#revFormDiv").remove(),
+      $("header").after(
+        `<div id='revFormDiv'><span id='reviewForm'>Please provide missing information</span></div>`
+      ));
+});
